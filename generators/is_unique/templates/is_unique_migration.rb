68b9efdaf92192e1cf64c70efa2b130a60413f80
eugenebolshakov <eugene.bolshakov@gmail.com>
2010-03-22 19:19:37 +0300

    updated migration template to calculate hashes for existing records

--- generators/is_unique/templates/is_unique_migration.rb
+++ generators/is_unique/templates/is_unique_migration.rb
@@ -2,6 +2,8 @@ class <%= file_name.camelcase %> < ActiveRecord::Migration
   def self.up
     add_column :<%= table_name %>, :<%= unique_hash_column %>, :integer
     add_index :<%= table_name %>, :<%= unique_hash_column %>
+    # Generate hashes for existing records
+    <%= class_name %>.all.collect { |r| r.save(false) }
   end
 
   def self.down
